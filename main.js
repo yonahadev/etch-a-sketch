8223fe4efd9fb95b05d0beba46d86f7f6ccd7782
thomasyonaha-mccoy <83908974+goat6@users.noreply.github.com>
2023-04-05 08:49:55 +0100

    successful grid size changing and painting squares

--- main.js
+++ main.js
@@ -11,13 +11,36 @@ function generateGrid() {
 }
 
 function paint() {
+    this.classList.add("painted");
+}
+
+function promptGridSize() { 
+    do {
+        gridSize = prompt("How many squares per row? (Max 64): ", gridSize);
+        if (gridSize > 64 || gridSize < 0) alert("Please enter a valid grid size!");
+    } while (gridSize > 64 || gridSize < 0);
+}
+
+function changeGridSize() { 
+    removeGridSquares()
+    promptGridSize()
+    generateGrid()
+    addEventListeners()
+}
+
+function removeGridSquares() { 
+    squares.forEach(square => square.remove());
 }
 
 function addEventListeners(){ 
-    squares = document.querySelectorAll(".paintSquare")
-    squares.forEach(square => square.addEventListener("hover",paint))
+    squares = document.querySelectorAll(".paintSquare");
+    console.log(squares);
+    squares.forEach(square => square.addEventListener("mouseover", paint));
 }
 
 let gridSize = 16;
 const gridWrapper = document.querySelector(".grid-wrapper");
-generateGrid()
+const gridSizeButton = document.querySelector(".grid-size-button")
+gridSizeButton.addEventListener("click",changeGridSize)
+generateGrid();
+addEventListeners();
